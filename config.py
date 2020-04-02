f018bca26ed2478d016d1170829b065628a76ce5
Pramod KC <49179488+Pramodbucky@users.noreply.github.com>
2020-04-02 17:55:04 +0545

    Delete config

--- config.py
+++
@@ -1,4 +0,0 @@
-useremail = 'pkc@growbydata.com'
-userpassword = 'Silencekhalifa99$'
-sender = 'pkc@growbydata.com'
-receiver = ['pramodshawn321@gmail.com', 'hakkuklaey123@gmail.com', 'infopramodsir@gmail.com', 'akshetashrestha719@gmail.com']
